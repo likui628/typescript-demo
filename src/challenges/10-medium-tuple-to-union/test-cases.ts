e25afe6d36b98ea7656e47c21d8e18b52bf18a36
likui628 <likui.it@gmail.com>
2021-11-27 11:36:58 +0800

    solution: 10-medium-tuple-to-union

--- src/challenges/10-medium-tuple-to-union/test-cases.ts
+++ src/challenges/10-medium-tuple-to-union/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Indexed Access Types
+ * https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+ */
+type TupleToUnion<T extends any[]> = T[number]
+
 type cases = [
   Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
   Expect<Equal<TupleToUnion<[123]>, 123>>,
