4d7a18b2ba281c8f3f703175b23e3bc7c200a15b
likui628 <likui.it@gmail.com>
2021-11-27 09:20:39 +0800

    solution: 3-medium-omit

--- src/challenges/3-medium-omit/test-cases.ts
+++ src/challenges/3-medium-omit/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Key Remapping via `as`
+ * https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#key-remapping-via-as
+ */
+type MyOmit<T, K extends keyof T> = { [P in keyof T as P extends K ? never : P]: T[P] }
+
 type cases = [
   Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
   Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>
