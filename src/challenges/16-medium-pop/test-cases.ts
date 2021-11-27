8ea8f7e3a1d4b81ba48ab7ef59c5c9cd0e0e54bb
likui628 <likui.it@gmail.com>
2021-11-27 20:14:52 +0800

    solution: 16-medium-pop

--- src/challenges/16-medium-pop/test-cases.ts
+++ src/challenges/16-medium-pop/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Variadic Tuple Types
+ * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-0.html#variadic-tuple-types
+ */
+type Pop<T extends any[]> = T extends [...infer T,infer _] ? T :never
+
 type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
   Expect<Equal<Pop<['a', 'b', 'c', 'd', ]>, ['a', 'b', 'c']>>,
