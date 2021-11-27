e655452382fa14ab08da0046ddac83a81916bdbd
likui628 <likui.it@gmail.com>
2021-11-27 10:12:11 +0800

    solution: 189-easy-awaited

--- src/challenges/189-easy-awaited/test-cases.ts
+++ src/challenges/189-easy-awaited/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Inferring Within Conditional Types
+ * https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#inferring-within-conditional-types
+ */
+type Awaited<T extends Promise<unknown>> = T extends Promise<infer R> ? (R extends Promise<unknown> ? Awaited<R> : R) : never;
+
 type X = Promise<string>
 type Y = Promise<{ field: number }>
 type Z = Promise<Promise<string | number>>
