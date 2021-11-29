7194adf41d9d011d919dab95dfd42d0c941bcd50
likui628 <likui.it@gmail.com>
2021-11-29 13:04:53 +0800

    solution: 610-medium-camelcase

--- src/challenges/610-medium-camelcase/test-cases.ts
+++ src/challenges/610-medium-camelcase/test-cases.ts
@@ -1,8 +1,15 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type CamelCase<S> = S extends `${infer H}-${infer T}`
+  ? T extends Capitalize<T>
+    ? `${H}-${CamelCase<T>}`
+    : CamelCase<`${H}${Capitalize<T>}`>
+  : S
+
 type cases = [
   Expect<Equal<CamelCase<'foo-bar-baz'>, 'fooBarBaz'>>,
   Expect<Equal<CamelCase<'foo-Bar-Baz'>, 'foo-Bar-Baz'>>,
+  Expect<Equal<CamelCase<'foo-Bar-baz'>, 'foo-BarBaz'>>,
   Expect<Equal<CamelCase<'foo-bar'>, 'fooBar'>>,
   Expect<Equal<CamelCase<'foo_bar'>, 'foo_bar'>>,
   Expect<Equal<CamelCase<'foo--bar----baz'>, 'foo-Bar---Baz'>>,
