48ee4c211b49b8aa18fcdc1579e6e7bbf5ae17b7
likui628 <likui.it@gmail.com>
2021-11-29 21:39:38 +0800

    fix: 116-medium-replace

--- src/challenges/116-medium-replace/test-cases.ts
+++ src/challenges/116-medium-replace/test-cases.ts
@@ -1,14 +1,13 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
 type Replace<S extends string, From extends string, To extends string> = From extends ''
-  ? S
-  : S extends `${infer H}${From}${infer L}`
-  ? `${H}${To}${L}`
-  : "To"
+? S : S extends `${infer H}${From}${infer L}`
+? `${H}${To}${L}` : S
   
 type cases = [
   Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
   Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
   Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
   Expect<Equal<Replace<'', '', ''>, ''>>,
 ]
