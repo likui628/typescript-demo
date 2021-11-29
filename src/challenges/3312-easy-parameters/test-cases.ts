a11c3d554353b96ab07a6a306ebd1088fb2d5c8b
likui628 <likui.it@gmail.com>
2021-11-29 10:23:40 +0800

    solution: 3312-easy-parameters

--- src/challenges/3312-easy-parameters/test-cases.ts
+++ src/challenges/3312-easy-parameters/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer Arg) => any ? Arg : never
+
 const foo = (arg1: string, arg2: number): void => {}
 const bar = (arg1: boolean, arg2: {a: 'A'}): void => {}
 const baz = (): void => {}
