3b75f2e8929f6f063467467cf3a68b6d6d95453f
likui628 <likui.it@gmail.com>
2021-11-29 20:09:55 +0800

    solution: 15-medium-last

--- src/challenges/15-medium-last/test-cases.ts
+++ src/challenges/15-medium-last/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type Last<T extends any[]> = T extends [...infer R, infer U] ? U : never
+
 type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
