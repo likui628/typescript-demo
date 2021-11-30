58e1d5486a76623bdb80c9c594588e4dcfc4d866
likui628 <likui.it@gmail.com>
2021-11-30 17:15:36 +0800

    solution: 106-medium-trimleft

--- src/challenges/106-medium-trimleft/test-cases.ts
+++ src/challenges/106-medium-trimleft/test-cases.ts
@@ -1,5 +1,9 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type Space = ' ' | '\n' | '\t'
+
+type TrimLeft<S extends string> = S extends `${Space}${infer Right}` ? TrimLeft<`${Right}`> : S
+
 type cases = [
   Expect<Equal<TrimLeft<'str'>, 'str'>>,
   Expect<Equal<TrimLeft<' str'>, 'str'>>,
