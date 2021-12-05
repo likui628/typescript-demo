736ca9388e826dfb4359aaae3d6647c8a0250833
likui628 <likui.it@gmail.com>
2021-12-05 20:12:04 +0800

    solution: 645 medium diff

--- src/challenges/645-medium-diff/test-cases.ts
+++ src/challenges/645-medium-diff/test-cases.ts
@@ -1,5 +1,22 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Intersection Types
+ * https://www.typescriptlang.org/docs/handbook/2/objects.html#intersection-types
+ * Key remapping in Mapped Types
+ * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#key-remapping-in-mapped-types
+ * Exclude
+ * https://www.typescriptlang.org/docs/handbook/utility-types.html#excludetype-excludedunion
+ */
+ type Diff<O, O1> = {
+    [P in keyof O | keyof O1 as Exclude<P, keyof O & keyof O1>]
+    : P extends keyof O
+      ? O[P]
+      : P extends keyof O1
+      ? O1[P]
+      : never
+  }
+
 type Foo = {
   name: string
   age: string
@@ -11,5 +28,6 @@ type Bar = {
 }
 
 type cases = [
-  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>
 ]
