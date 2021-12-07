4ac4476ab6f95670c8d6fc8f195b195f6a7cdab2
likui628 <likui.it@gmail.com>
2021-12-07 16:18:21 +0800

    solution: 2688-medium-startswith

--- src/challenges/2688-medium-startswith/test-cases.ts
+++ src/challenges/2688-medium-startswith/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type StartsWith<T extends string, U extends string> = T extends `${U}${infer Rest}` ? true : false
+
 type cases = [
   Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
   Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
