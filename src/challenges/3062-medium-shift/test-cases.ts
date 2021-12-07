5e1061ac967e3589b152abf591b616c51a00a531
likui628 <likui.it@gmail.com>
2021-12-07 22:10:24 +0800

    solution: 3062-medium-shift

--- src/challenges/3062-medium-shift/test-cases.ts
+++ src/challenges/3062-medium-shift/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type Shift<T extends any[]> = T extends [infer _, ...infer Rest] ? [...Rest] : T
+
 type cases = [
   Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
   Expect<Equal<Shift<['a', 'b', 'c', 'd', ]>, ['b', 'c', 'd']>>,
