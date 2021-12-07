58a6b480a974c7a2c32eec478c33c1f32f71b3cc
likui628 <likui.it@gmail.com>
2021-12-07 16:18:44 +0800

    solution: 2693-medium-endswith

--- src/challenges/2693-medium-endswith/test-cases.ts
+++ src/challenges/2693-medium-endswith/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type EndsWith<T extends string, U extends string> = T extends `${infer Rest}${U}` ? true : false
+
 type cases = [
   Expect<Equal<EndsWith<'abc', 'bc'>, true>>,
   Expect<Equal<EndsWith<'abc', 'abc'>, true>>,
