f6fc3075e8dfe0a281e96efcfbbd82c460039f9c
likui628 <likui.it@gmail.com>
2021-12-08 09:42:43 +0800

    solution: 2793-medium-mutable

--- src/challenges/2793-medium-mutable/test-cases.ts
+++ src/challenges/2793-medium-mutable/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type Mutable<T> = { -readonly [P in keyof T]: T[P] }
+
 interface Todo1 {
   title: string
   description: string
