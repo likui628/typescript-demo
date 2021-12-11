01a2c6630b11947ab8fe55ff8df70b1c3ddff5ae
likui628 <likui.it@gmail.com>
2021-12-11 12:11:49 +0800

    solution: 12-medium-chainable-options

--- src/challenges/12-medium-chainable-options/test-cases.ts
+++ src/challenges/12-medium-chainable-options/test-cases.ts
@@ -1,5 +1,11 @@
 import { Alike, Expect } from '@type-challenges/utils'
 
+
+type Chainable<O = {}> = {
+    option<K extends string, V>(key: K, value: V): Chainable<O & { [P in K]: V }>
+    get(): O
+  }
+  
 declare const a: Chainable
 
 const result = a
