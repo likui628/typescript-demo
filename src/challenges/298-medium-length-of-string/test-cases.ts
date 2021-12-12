70c158d84a0304c9cecbacc7f788baf596a5492f
likui628 <likui.it@gmail.com>
2021-12-12 16:32:34 +0800

    solution: 298-medium-length-of-string

--- src/challenges/298-medium-length-of-string/test-cases.ts
+++ src/challenges/298-medium-length-of-string/test-cases.ts
@@ -1,5 +1,9 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type LengthOfString<S extends string, T extends any[] = []> = S extends `${infer H}${infer L}`
+  ? LengthOfString<L, [...T, H]>
+  : T['length']
+  
 type cases = [
   Expect<Equal<LengthOfString<''>, 0>>,
   Expect<Equal<LengthOfString<'kumiko'>, 6>>,
