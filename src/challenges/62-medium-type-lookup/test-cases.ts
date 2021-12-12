0f1cc56dd4929da6583b672bb4c07cd3f8c15e3b
likui628 <likui.it@gmail.com>
2021-12-12 19:46:07 +0800

    solution: 62-medium-type-lookup

--- src/challenges/62-medium-type-lookup/test-cases.ts
+++ src/challenges/62-medium-type-lookup/test-cases.ts
@@ -1,5 +1,12 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Distributive Conditional Types
+ * https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+ * When conditional types act on a generic type, they become distributive when given a union type. 
+ */
+type LookUp<U, T> = U extends { type: T } ? U : never
+
 interface Cat {
   type: 'cat'
   breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
