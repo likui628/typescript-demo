476efd5fb35df4a4cadb59d33c8bd51357b985ad
likui628 <likui.it@gmail.com>
2022-01-21 16:57:46 +0800

    update 14-easy-first

--- src/challenges/14-easy-first/test-cases.ts
+++ src/challenges/14-easy-first/test-cases.ts
@@ -1,7 +1,8 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
 // type First<T extends Array<any>> = T extends Array<never> ? never : T[0]
-type First<T extends any[]> = T extends [] ? never : T[0]
+// type First<T extends any[]> = T extends [] ? never : T[0]
+type First<T extends any[]> = T extends [infer F, ...infer _] ? F : never
 
 type cases = [
   Expect<Equal<First<[3, 2, 1]>, 3>>,
